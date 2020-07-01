375e40fd0fbecb0f0d41608b40568501390b843a
Anne Thor <anne.thor@gmail.com>
2020-07-01 00:04:06 -0700

    Updates

---
+++ precache-manifest.547aaae55e744e4a48a0b820d6e13325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd461eeca6f8695e09a341034d0af6a",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "ed02b8c8bfd543a01d74",
+    "url": "/pomodoro-clock/static/css/main.a2540f4d.chunk.css"
+  },
+  {
+    "revision": "4dfe0c8a4d0ed152dcb0",
+    "url": "/pomodoro-clock/static/js/2.5ca0f28d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.5ca0f28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed02b8c8bfd543a01d74",
+    "url": "/pomodoro-clock/static/js/main.db68a2dd.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
